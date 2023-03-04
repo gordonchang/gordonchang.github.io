bc2746a31023f91a7726734d384ebc75ae1db108
張家郡 <91380992+gordonchang@users.noreply.github.com>
2023-03-04 20:55:44 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+// 獲取圖片元素
+var img = document.getElementById("dog");
+
+// 設置初始位置
+var x = 0;
+
+// 設置移動速度
+var speed = 0.5;
+
+// 設置移動方向
+var directionX = 1;
+
+
+// 設置移動函數
+function moveImage() {
+    // 計算新的位置
+    x = x + (speed * directionX);
+
+
+    // 判斷是否需要改變移動方向
+    if (x > window.innerWidth - img.width || x < 0) {
+        directionX = -directionX;
+    }
+
+
+    // 更新圖片位置
+    img.style.left = x + "px";
+
+    // 設置下一次移動
+    window.requestAnimationFrame(moveImage);
+}
+
+// 開始移動
+moveImage();
